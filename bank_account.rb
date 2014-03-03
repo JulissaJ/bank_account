f4f95d73650379c0e9a3d76056a8f84f55f31dfe
Barry Lam <blam789@outlook.com>
2014-03-03 17:09:06 -0500

    initial commit

---
+++ bank_account.rb
@@ -0,0 +1,112 @@
+
+class BankTransaction
+  attr_reader :date, :amount, :description, :account
+  def initialize(date, amount, description, account)
+    @date = date
+    @amount = amount
+    @description = description
+    @account = account
+  end
+
+  def to_s
+    "Date: #{@date} - amount: #{@amount} - #{@description} - #{@account}"
+  end
+
+  def debit?
+    @amount.to_f < 0
+  end
+
+  def credit?
+    @amount.to_f > 0
+  end
+end
+
+class BankAccount
+  attr_reader :account_type, :starting_balance, :ending_balance
+  def initialize(type, starting_balance)
+    @account_type = type
+    @starting_balance = starting_balance.to_f
+    @ending_balance = starting_balance.to_f
+    @transactions = []
+  end
+
+  def to_s
+    "#{@account_type} - starting balance: #{@starting_balance} - ending balance: #{@ending_balance}"
+  end
+
+  def add_transactions(transactions)
+    transactions.each do |trans|
+      @transactions << trans if trans.account == @account_type
+      @ending_balance += trans.amount.to_f
+    end
+  end
+
+  def summary
+    @transactions.map { |trans| trans.description }
+  end
+
+  # def report
+  #   #return a hash of starting bal, ending bal, and transactions (value = array of hashes)
+  #   hash = {}
+  #   hash[:starting] = @starting_balance
+  #   hash[:ending] = @ending_balance
+  #   hash[:transactions] = []
+  #   @transactions.each {|trans| hash[:transactions] << trans}
+  # end
+
+  def currency(num)
+    "$#{'%.2f' % (num)}"
+  end
+
+  # def date(date_string)
+  #   month = date_string[0..1]
+  #   day = date_string[3..4]
+  #   year = date_string[-4..-1]
+  #   date = Time.new(year, month, day)
+  #   date.strftime('%m/%d/%Y')
+  # end
+
+  def report
+    #return an array of lines of text suitable for html
+    array = []
+    array << "==== #{@account_type} ====\n\n"
+    array << "Starting Balance: #{currency(@starting_balance)}"
+    array << "Ending Balance:   #{currency(@ending_balance)}\n\n"
+    @transactions.each do |trans|
+      if trans.credit?
+        type = 'CREDIT'
+      elsif trans.debit?
+        type = 'DEBIT'
+      end
+      array << "#{currency(trans.amount)}  \t#{type}\t#{trans.date} - #{trans.description}"
+    end
+    array << "\n========\n"
+  end
+end
+
+require 'csv'
+
+def create_accounts(csv)
+  accounts = []
+  CSV.foreach(csv, headers: true) do |row|
+    accounts << BankAccount.new(row[0], row[1])
+  end
+  accounts
+end
+
+#Date,Amount,Description,Account
+def read_transactions(csv)
+  transactions = []
+  CSV.foreach(csv, headers: true) do |row|
+    transactions << BankTransaction.new(row[0], row[1], row[2], row[3])
+  end
+  transactions
+end
+
+accounts = create_accounts('balances.csv')
+puts accounts
+accounts.each {|acc| acc.add_transactions(read_transactions('bank_data.csv'))}
+
+accounts.each do |acc|
+  puts acc.report
+end
